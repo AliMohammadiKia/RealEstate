5cff8a3c7e35d74ac295a69bae3e1456b2cc54bd
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2023-02-20 21:18:42 +0330

    update api key for request limit

--- src/js/common/fetchApi.js
+++ src/js/common/fetchApi.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://bayut.p.rapidapi.com/";
-const apiKey = "f32e57abfamsh63eea3f0c2844f3p167155jsnbac6af7ebe2c";
+const apiKey = "36b5219f26msh7cb33c4b83b64cbp1307bejsn44fd537f40ef";
 
 export const fetchApi = async (url) => {
   const response = await fetch(baseUrl + url, {
